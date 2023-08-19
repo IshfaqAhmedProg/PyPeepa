f6e198037796eb66e269e94473b10194c7b5cf0a
IshfaqAhmedProg <manslayeriam@gmail.com>
2023-08-19 16:48:50 +0600

    version 0.3

---
+++ __init__.py
@@ -0,0 +1,34 @@
+from .utilities import (
+    getFilePath,
+    askYNQuestion,
+    getInputFilePath,
+    printArray,
+    selectOptionQuestion,
+    signature,
+    asyncListDir,
+    asyncReadJSON,
+    createDirectory,
+    readData,
+    writeFile,
+    processCSVInChunks,
+)
+
+
+__author__ = "Ishfaq Ahmed"
+__email__ = "ishfaqahmed0837@gmail.com"
+__version__ = "0.3"
+__description__ = ("Custom built utilities for general use",)
+__all__ = (
+    "processCSVInChunks",
+    "getFilePath",
+    "askYNQuestion",
+    "getInputFilePath",
+    "printArray",
+    "selectOptionQuestion",
+    "signature",
+    "asyncListDir",
+    "asyncReadJSON",
+    "createDirectory",
+    "readData",
+    "writeFile",
+)
--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setup(
     name="pypeepa",
-    version="0.1",
+    version="0.3",
     author="Ishfaq Ahmed",
     author_email="ishfaqahmed0837@gmail.com",
     description="Custom built utilities for general use",
