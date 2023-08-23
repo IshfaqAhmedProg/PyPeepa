062d7ba08d261627b219be947e2eb2238ca0a624
IshfaqAhmedProg <manslayeriam@gmail.com>
2023-08-23 18:07:13 +0600

    fix: v2.4

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setup(
     name="pypeepa",
-    version="2.3",
+    version="2.4",
     author="Ishfaq Ahmed",
     author_email="ishfaqahmed0837@gmail.com",
     description="Custom built utilities for general use",
