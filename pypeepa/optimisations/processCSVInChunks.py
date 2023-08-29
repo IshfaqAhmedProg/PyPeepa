f94874bc21b723852e915e6a859ee9a2f1632535
IshfaqAhmedProg <manslayeriam@gmail.com>
2023-08-29 22:50:33 +0600

    fix: encoding errors on processCSVInChunks v3.5

--- pypeepa/optimisations/processCSVInChunks.py
+++ pypeepa/optimisations/processCSVInChunks.py
@@ -26,16 +26,21 @@ def processCSVInChunks(
     @param:`chunk_size`: (Optional) Size of chunks to work with\n
     @param:`hide_progress_bar`: (Optional) Set to True if you dont want the progress bar that comes with this\n
     @return:
-        If the func has return values than return the values in a list else return None\n
+        If the process_function has return values than return the values in a list else return None\n
     """
     # Create a generator to read the CSV file in chunks
-    chunk_reader = pd.read_csv(csv_file, chunksize=chunk_size, low_memory=False)
+    chunk_reader = pd.read_csv(
+        csv_file, chunksize=chunk_size, low_memory=False, encoding_errors="ignore"
+    )
 
     # Process each chunk and concatenate the results
     processed_chunks = []
     # Show progress bar by default
     # Count total number of lines in the csv_file
-    total_chunks = int(sum(1 for row in open(csv_file, "r")) / chunk_size)
+    total_chunks = int(
+        sum(1 for row in open(csv_file, "r", encoding="cp437", errors="ignore"))
+        / chunk_size
+    )
     for chunk in (
         chunk_reader
         if hide_progress_bar
