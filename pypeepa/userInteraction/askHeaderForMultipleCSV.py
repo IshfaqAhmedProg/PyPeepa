fdd4a3743891c7952f579c225e896b2e30d2370b
IshfaqAhmedProg <manslayeriam@gmail.com>
2023-09-29 18:14:42 +0600

    fix:askHeaderformultiplecsv fixed v5.0

--- pypeepa/userInteraction/askHeaderForMultipleCSV.py
+++ pypeepa/userInteraction/askHeaderForMultipleCSV.py
@@ -1,4 +1,4 @@
-from typing import List, Tuple
+from typing import List, Tuple, Optional
 from pandas import read_csv
 import numpy
 import os
@@ -6,10 +6,13 @@ from pypeepa.userInteraction.askSelectOptionQuestion import askSelectOptionQuest
 from pypeepa.userInteraction.printArray import printArray
 
 
-def askHeaderForMultipleCSV(csv_list: List[str], csv_dir: str) -> List[Tuple[str, str]]:
+def askHeaderForMultipleCSV(
+    csv_list: List[str], csv_dir: str, label: Optional[str] = "values"
+) -> List[Tuple[str, str]]:
     """Ask the user to select headers for many csv files, it will skip repeated sets of headers
     @param:`csv_list`:List of csv file names.
     @param:`csv_dir`:The directory of the csvs.
+    @param:`label`:A general label which will be displayed to the user for the specific type of column.
     @return: Returns a list of tuples containing the csv file name and the selected header name
     """
     files_and_header = []
@@ -23,7 +26,7 @@ def askHeaderForMultipleCSV(csv_list: List[str], csv_dir: str) -> List[Tuple[str
         if not numpy.array_equal(numpy.array(prev_cols), numpy.array(current_columns)):
             printArray(current_columns)
             col_index = askSelectOptionQuestion(
-                question=f"Enter the index of the column you want to match.",
+                question=f"Enter the index of the column containing the {label}.",
                 min=1,
                 max=len(current_columns),
             )
--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setup(
     name="pypeepa",
-    version="4.9",
+    version="5.0",
     author="Ishfaq Ahmed",
     author_email="ishfaqahmed0837@gmail.com",
     description="Custom built utilities for general use",
